c37355fc0557f0466c8b081ff81d3cb08ffb7010
Robert Marinho <robstermarinho@gmail.com>
2019-08-11 14:23:13 -0300

    Post List Item and Coments

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import profile from "./assets/robert.jpg";
 import Header from "./components/Header";
+import PostList from "./components/PostList";
 
 const App = () => {
   return (
     <>
       <Header />
+      <PostList />
     </>
   );
 };
